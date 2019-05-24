333c1bdc3ecec8420901a7c3ad8af793fdd441ce
welgt <welgt@git.github.com>
2019-05-24 15:26:55 -0300

    Add classes

--- src/connorsRedemption/Item.java
+++ src/connorsRedemption/Item.java
@@ -1,14 +1,19 @@
 package connorsRedemption;
 
+import java.applet.AudioClip;
 import java.awt.Image;
 
 public class Item {
 	
+	private int texteHerança;
 	private String nome;
 	private Image skin;
 	private float posItemX;
 	private float posItemY;
 	private int raioColisao;
+	private AudioClip audioItem; // som ao coletar o item(ainda vou estudar como se faz isso
+	                             // so estou colocando pra nao me esquecer!)
+	
 
 	public Item (String nome,Image skin, float posX, float posY) {
 		this.nome = nome;
@@ -21,7 +26,7 @@ public class Item {
 	public boolean detectarColisao(String nomeObjeto, float posXobjeto, float posYobjeto) {
 		
 		if(this.posItemX == posXobjeto - (this.raioColisao/2) || this.posItemY == posYobjeto - (this.raioColisao/2)){
-			
+			// tocar audioClip audioItem
 			return true;
 		}
 		return false;
